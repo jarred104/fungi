a92ce975238e168b483e640b46cb7f72062d8ca7
jarred104 <brouhardjr@gmail.com>
2019-10-22 22:19:24 -0700

    restructured files, need to fix html

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# import datetime
+import datetime
 import json
 import sqlite3
 import csv
 from flask import Flask, jsonify, request, render_template
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func, Column, String
-# from sqlalchemy.ext.automap import automap_base
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, Column, String
+from sqlalchemy.ext.automap import automap_base
 
-# engine = create_engine("sqlite:////C:/Users/jarredb/Desktop/test/test_db")
+engine = create_engine("sqlite:///fungi_db.sqlite")
 
 # # reflect an existing database into a new model
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 # # references to each table
-# Table = Base.classes.test_table
+Table = Base.classes.findings
 
 
 # # create instance of Flask app
@@ -38,35 +38,38 @@ if __name__ == "__main__":
 
 
 # #create a route for the data
-# @app.route("/api")
-# def api():
-# #     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api")
+def api():
+#     # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-#     """Return a list of all results"""
-# #     # Query all test_db
-#     results = session.query(
-#         Table.id,
-#         Table.date,
-#         Table.type,
-#         Table.notes,
-#         Table.lat,
-#         Table.lon).all()
+    """Return a list of all results"""
+    # Query all test_db
+    results = session.query(
+        Table.id,
+        Table.date,
+        Table.type,
+        Table.aka,
+        Table.finder,       
+        Table.notes,
+        Table.lat,
+        Table.lon,
+        Table.ref).all()
 
-#     session.close()
+    session.close()
 
     # Create a dictionary from the row data and append to a list
-#     test_db = []
-#     for id, date, type, notes, lat, lon in results:
-#         test_dict = {}
-#         test_dict["id"] = id
-#         test_dict["date"] = date
-#         test_dict["type"] = type
-#         test_dict["notes"] = notes
-#         test_dict["lat"] = lat
-#         test_dict["lon"] = lon
-#         test_db.append(test_dict)
-#     return jsonify(test_db)
+    test_db = []
+    for id, date, type, notes, lat, lon in results:
+        test_dict = {}
+        test_dict["id"] = id
+        test_dict["date"] = date
+        test_dict["type"] = type
+        test_dict["notes"] = notes
+        test_dict["lat"] = lat
+        test_dict["lon"] = lon
+        test_db.append(test_dict)
+    return jsonify(test_db)
 
 
 # if __name__ == "__main__":
